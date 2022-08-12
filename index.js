da0f7c149f32e661b0ba0197999e68ce7a6f7827
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-12 12:37:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+//build function writeCards() with 2 arguments...
+const array = [];
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;   
+    }
+    return array;
+}
+function countDown(i){
+    while ( i>=0 ){
+        console.log(i);
+        i--;
+    }
+}
